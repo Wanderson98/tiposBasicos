68331a7350f520a75c9095a41ecebbe09cbe9352
Wanderson Andrade <wmandrade81@gmail.com>
2021-10-22 13:48:42 -0300

    Adicionar arquivos de projeto.

---
+++ tiposBasicos/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace tiposBasicos {
+    class Program {
+        static void Main(string[] args) {
+            bool completo = true;
+            char genero = 'M';
+            char letra = '\u0041';
+            byte n1 = 96;
+            int n2 = 900;
+            int n3 = 2141253647;
+            long n4 = 2147985648L;
+            float n5 = 2.5f;
+            double n6 = 1.5;
+            String nome = "Joana Lins";
+            Object obj1 = "Bruno Farias";
+            Object obj2 = 4.5f;
+            Console.WriteLine(completo);
+            Console.WriteLine(genero);
+            Console.WriteLine(letra);
+            Console.WriteLine(n1);
+            Console.WriteLine(n2);
+            Console.WriteLine(n3);
+            Console.WriteLine(n4);
+            Console.WriteLine(n5);
+            Console.WriteLine(n6);
+            Console.WriteLine(nome);
+            Console.WriteLine(obj1);
+            Console.WriteLine(obj2);
+        }
+    }
+}
